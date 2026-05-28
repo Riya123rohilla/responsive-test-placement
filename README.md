01978e76000ae8d46260662617c35bb77cc9d59a
Riya Rohilla <177570988+Riya123rohilla@users.noreply.github.com>
2026-05-28 09:43:21 +0530

    files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # responsive-test-placement
+# Frontend Assessment — Responsive Landing Page
+
+A responsive frontend project built using **HTML5, CSS3, and Vanilla JavaScript (ES6+)** based on the provided Figma design.
+
+---
+
+## 🚀 Live Demo
+
+Add your deployed link here:
+
+```bash
---
+++ script.js
@@ -0,0 +1,21 @@
+
+const swiper = new Swiper(".heroSwiper", {
+
+//   loop: true,
+
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
